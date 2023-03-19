f9cbedb9b9c083d0302166d4f8aa7570768d25b9
tipp123 <tipp123@gmail.com>
2023-03-19 18:19:09 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        String input;
+        System.out.println("Alustan mängu!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sisesta oma nimi: ");
+        input = scan.nextLine();
+        User user1 = new User(input);
+        System.out.println("Tere, " + user1.getName() + "!");
         System.out.println("Hello world!");
     }
 }
--- src/Question.java
+++ src/Question.java
@@ -1,2 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
 public class Question {
+    private String question;
+    private List<String> answers;
+    private String correctAnswer;
+
+    Question (String question, List<String> answers, String correctAnswer) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
 }
