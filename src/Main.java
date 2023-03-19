534e84673fb835fbb6282baefe67ec4f6f506682
tipp123 <tipp123@gmail.com>
2023-03-19 22:10:56 +0200

    Added general structure for reading and presenting questions

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,63 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.List;
 public class Main {
+    static List<Question> questions = new ArrayList<>();
+    static Scanner scan = new Scanner(System.in);
+    static User user;
     public static void main(String[] args) {
         String input;
-        System.out.println("Alustan mängu!");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Sisesta oma nimi: ");
+
+
+        System.out.print("Sisesta oma nimi: ");
         input = scan.nextLine();
-        User user1 = new User(input);
-        System.out.println("Tere, " + user1.getName() + "!");
-        System.out.println("Hello world!");
+        user = new User(input);
+        System.out.println("Tere, " + user.getName() + "!");
+        System.out.println("Alustame mängu..");
+        System.out.println();
+
+        readQuestions();
+        presentQuestions();
     }
+
+    static void readQuestions() {
+        Question question1 = new Question("What is the capital of France?", Arrays.asList("Paris", "Berlin", "London", "Madrid"), "Paris");
+        Question question2 = new Question("What is the largest planet in our solar system?", Arrays.asList("Venus", "Mars", "Jupiter", "Saturn"), "Jupiter");
+        questions.add(question1);
+        questions.add(question2);
+    }
+
+    static void presentQuestions() {
+        String answer;
+        for (Question question : questions) {
+            System.out.println(question.getQuestion());
+            presentAnswers(question);
+            System.out.println();
+            System.out.print("Sisesta vastus: ");
+            answer = scan.nextLine();
+            if (answer.equals(question.getCorrectAnswer()))
+            {
+                System.out.println("Õige vastus!");
+                user.addScore(1);
+                System.out.println("Sinu skoor on: " + user.getScore());
+                System.out.println();
+            }
+            else
+            {
+                System.out.println("See on kahjuks vale vastus");
+                System.out.println("Sinu skoor on: " + user.getScore());
+                System.out.println();
+            }
+
+        }
+    }
+
+    static void presentAnswers(Question question) {
+        for (String answer : question.getAnswers()) {
+            System.out.println(answer);
+        }
+
+    }
+
 }
